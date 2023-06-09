886085ec795e6a7449acffc64f2708bb3ff8209b
alaoddine yaden <alaoddine.yaden@usmba.ac.ma>
2023-06-10 00:56:37 +0100

    change code

--- app.py
+++ app.py
@@ -1,120 +1,15 @@
-# from flask import Flask, send_file, render_template
-# from flask.globals import g
-# import sqlite3
-
-# app = Flask(__name__)
-# app.config['DATABASE'] = 'emails.db'
-
-# # Create the emails table if it doesn't exist
-# def create_emails_table():
-#     with app.app_context():
-#         conn = get_db()
-#         cursor = conn.cursor()
-#         cursor.execute('''CREATE TABLE IF NOT EXISTS emails
-#                           (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT)''')
-#         conn.commit()
-
-# # Create the requests table if it doesn't exist
-# def create_requests_table():
-#     with app.app_context():
-#         try:
-#             conn = get_db()
-#             cursor = conn.cursor()
-#             cursor.execute('''CREATE TABLE IF NOT EXISTS requests
-#                               (id INTEGER PRIMARY KEY AUTOINCREMENT, email_id INTEGER UNIQUE, count INTEGER DEFAULT 0,
-#                               FOREIGN KEY(email_id) REFERENCES emails(id))''')
-#             conn.commit()
-#             conn.close()
-#         except sqlite3.Error as e:
-#             print(f"Error creating 'requests' table: {e}")
-
-
-# # Insert emails from file into the database
-# def insert_emails():
-#     with app.app_context():
-#         conn = get_db()
-#         cursor = conn.cursor()
-#         with open('emails.txt', 'r') as file:
-#             emails = file.read().splitlines()
-#             email_data = [(email,) for email in emails]
-#             cursor.executemany('''INSERT INTO emails (email) VALUES (?)''', email_data)
-#             conn.commit()
-
-# # Get the database connection
-# def get_db():
-#     db = getattr(g, '_database', None)
-#     if db is None:
-#         db = g._database = sqlite3.connect(app.config['DATABASE'])
-#     return db
-
-# # Close the database connection at the end of the request
-# @app.teardown_appcontext
-# def close_db(exception):
-#     db = getattr(g, '_database', None)
-#     if db is not None:
-#         db.close()
-
-# @app.route('/')
-# def hello_world():
-#     return 'Hello from Flask!'
-
-# @app.route('/image/<int:email_id>')
-# def track_image_request(email_id):
-#     with app.app_context():
-#         conn = get_db()
-#         cursor = conn.cursor()
-
-#         # Increment the view count
-#         view_count = increment_view_count(cursor, email_id)
-
-#         # Retrieve the email based on the email ID
-#         email = get_email(cursor, email_id)
-
-#         cursor.close()
-
-#         if email:
-#             return send_file('gg.gif', mimetype='image/gif')
-#         else:
-#             return 'Email not found', 404
-
-# def increment_view_count(cursor, email_id):
-#     cursor.execute('''INSERT OR IGNORE INTO requests (email_id, count)
-#                       VALUES (?, 0)''', (email_id,))
-#     if cursor.rowcount > 0:
-#         # A new row was inserted, increment the ID
-#         cursor.execute('''UPDATE sqlite_sequence SET seq = seq + 1 WHERE name = 'requests' ''')
-#     cursor.execute('''UPDATE requests SET count = count + 1 WHERE email_id = ?''', (email_id,))
-#     cursor.connection.commit()
-
-#     cursor.execute('''SELECT count FROM requests WHERE email_id = ?''', (email_id,))
-#     view_count = cursor.fetchone()[0]
-
-#     return view_count
-
-# def get_email(cursor, email_id):
-#     cursor.execute('''SELECT email FROM emails WHERE id = ?''', (email_id,))
-#     email = cursor.fetchone()
-#     if email:
-#         return email[0]
-#     else:
-#         return None
-
-# if __name__ == '__main__':
-#     create_emails_table()
-#     create_requests_table()
-#     insert_emails()
-#     app.run()
-
 from flask import Flask, send_file, render_template
 from flask.globals import g
 import mysql.connector
+import time
+import tempfile
 
 app = Flask(__name__)
 app.config['DATABASE'] = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '',
-    'database': 'emails'
+    'host': 'cuddy.mysql.pythonanywhere-services.com',
+    'user': 'cuddy',
+    'password': 'V8RC8gm@TfpJqDk',
+    'database': 'cuddy$emails'
 }
 
 # Create the emails table if it doesn't exist
@@ -140,16 +35,29 @@ def create_requests_table():
         except mysql.connector.Error as e:
             print(f"Error creating 'requests' table: {e}")
 
+
 # Insert emails from file into the database
 def insert_emails():
     with app.app_context():
         conn = get_db()
         cursor = conn.cursor()
-        with open('emails.txt', 'r') as file:
+        with open('/home/cuddy/mysite/emails.txt', 'r') as file:
             emails = file.read().splitlines()
             email_data = [(email,) for email in emails]
-            cursor.executemany('''INSERT INTO emails (email) VALUES (%s)''', email_data)
+            cursor.executemany('''INSERT INTO cuddy$emails.emails (email) VALUES (%s)''', email_data)
             conn.commit()
+
+            # Wait until the emails are inserted
+            expected_count = len(emails)
+            while True:
+                cursor.execute('''SELECT COUNT(*) FROM cuddy$emails.emails''')
+                count = cursor.fetchone()[0]
+                if count == expected_count:
+                    break
+                time.sleep(1)
+
+        conn.close()
+
 
 # Get the database connection
 def get_db():
@@ -169,6 +77,48 @@ def close_db(exception):
 def hello_world():
     return 'Hello from Flask!'
 
+@app.route('/insert')
+def insert():
+    try:
+        insert_emails()
+        return 'inserted'
+    except mysql.connector.Error as e:
+            print(f"Error insert 'emails' table: {e}")
+            return f"error : {e}"
+
+@app.route('/download_emails')
+def download_emails():
+    with app.app_context():
+        conn = get_db()
+        cursor = conn.cursor()
+
+        # Retrieve all emails from the database
+        cursor.execute('''SELECT id, email FROM emails''')
+        rows = cursor.fetchall()
+
+        conn.close()
+
+        # Create a string to store the emails and IDs
+        email_text = ""
+        for row in rows:
+            email_id = row[0]
+            email = row[1]
+            email_text += f"{email_id}: {email}\n"
+
+        # Create a temporary file and write the email text into it
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(email_text.encode())
+            temp_file.flush()
+
+            # Return the emails as a text file
+            return send_file(
+                temp_file.name,
+                mimetype="text/plain",
+                as_attachment=True,
+                attachment_filename="emails.txt"
+            )
+
+
 @app.route('/image/<int:email_id>')
 def track_image_request(email_id):
     with app.app_context():
@@ -178,17 +128,20 @@ def track_image_request(email_id):
         # Increment the view count
         view_count = increment_view_count(cursor, email_id)
 
+
         # Retrieve the email based on the email ID
         email = get_email(cursor, email_id)
-
+        conn.commit()
         cursor.close()
-
+        conn.close()
         if email:
-            return send_file('gg.gif', mimetype='image/gif')
+            return send_file('/home/cuddy/mysite/gg.gif', mimetype='image/gif')
         else:
             return 'Email not found', 404
 
+
 def increment_view_count(cursor, email_id):
+
     cursor.execute('''INSERT INTO requests (email_id, count)
                       VALUES (%s, 1)
                       ON DUPLICATE KEY UPDATE count = count + 1''', (email_id,))
@@ -197,10 +150,10 @@ def increment_view_count(cursor, email_id):
     if cursor.rowcount > 0:
         view_count = 1
     else:
-        cursor.execute('''UPDATE requests SET count = count + 1 WHERE email_id = %s''', (email_id,))
-        cursor.connection.commit()
         cursor.execute('''SELECT count FROM requests WHERE email_id = %s''', (email_id,))
+
         view_count = cursor.fetchone()[0]
+
 
     return view_count
 
@@ -218,5 +171,36 @@ def get_email(cursor, email_id):
 if __name__ == '__main__':
     create_emails_table()
     create_requests_table()
-    insert_emails()
     app.run()
+
+
+
+
+
+
+# # A very simple Flask Hello World app for you to get started with...
+
+# from flask import Flask, send_file
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello from Flask!'
+
+
+# @app.route('/image')
+# def track_image_request():
+#     # Increment the view count
+#     # view_count = 0
+#     # with open('/home/cuddy/mysite/view_count.txt', 'w') as f:
+#     #     f.write(str(view_count))
+#     with open('/home/cuddy/mysite/view_count.txt', 'r') as f:
+#         view_count = int(f.read().strip())
+#     view_count += 1
+#     with open('/home/cuddy/mysite/view_count.txt', 'w') as f:
+#         f.write(str(view_count))
+
+#     # Return a 1x1 pixel PNG image
+#     return send_file('/home/cuddy/mysite/gg.gif', mimetype='image/gif')
+
